9b535df0c182df04daae1c29d874c16cb338a0a3
Vengal Rao Guttha <119367623+sfc-gh-vguttha@users.noreply.github.com>
2025-03-31 18:46:39 -0700

    Add OTEL context propagation support for executors  (#3)
    * Add OTEL context propagation support for executors
    
    * refactor
    
    * add changelog
    
    * refactor
    
    * refactor

--- dbt_common/context.py
+++ dbt_common/context.py
@@ -4,6 +4,8 @@ from typing import List, Mapping, Optional, Iterator
 
 from dbt_common.constants import PRIVATE_ENV_PREFIX, SECRET_ENV_PREFIX
 from dbt_common.record import Recorder
+from opentelemetry.context.context import Context
+from opentelemetry import context as opentelemetry_context
 
 
 class CaseInsensitiveMapping(Mapping[str, str]):
@@ -81,6 +83,8 @@ def reliably_get_invocation_var() -> ContextVar[InvocationContext]:
 
     return invocation_var
 
+def set_otel_context(context: Context):
+    opentelemetry_context.attach(context)
 
 def set_invocation_context(env: Mapping[str, str]) -> None:
     invocation_var = reliably_get_invocation_var()
@@ -91,7 +95,6 @@ def get_invocation_context() -> InvocationContext:
     invocation_var = reliably_get_invocation_var()
     ctx = invocation_var.get()
     return ctx
-
 
 def try_get_invocation_context() -> Optional[InvocationContext]:
     try:
